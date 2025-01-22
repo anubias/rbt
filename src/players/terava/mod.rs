38b8dae93bc9f1ef79e0dab3c1e2258cccd0e065
Antti Terävä <antti.terava@wapice.com>
2025-01-22 11:24:16 +0200

    Added structure for decision logic with the help of CoPilot.

--- src/players/terava/mod.rs
+++ src/players/terava/mod.rs
@@ -1,27 +1,55 @@
 use super::player::*;
 
-pub struct PlAgiAntti {
-    fired: bool,
-}
+pub struct PlAgiAntti {}
 
 impl PlAgiAntti {
     pub fn new() -> Self {
-        Self { fired: false }
+        Self {}
+    }
+
+    fn evaluate_threat(&self, _context: &Context) -> u32 {
+        // TODO: Implement logic to assess the threat level
+        rand::random::<u32>() % 2
+    }
+
+    fn evaluate_opportunity(&self, _context: &Context) -> u32 {
+        // TODO: Implement logic to assess the opportunity level
+        rand::random::<u32>() % 2
+    }
+
+    fn defensive_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for defensive actions
+        Action::Move(Direction::Backward)
+    }
+
+    fn offensive_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for offensive actions
+        Action::Fire
+    }
+
+    fn exploratory_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for exploratory actions
+        Action::Scan(ScanType::Directional(Orientation::East))
     }
 }
 
 impl Player for PlAgiAntti {
-    fn act(&mut self, _context: &Context) -> Action {
-        if self.fired {
-            self.fired = false;
-            return Action::Rotate(Rotation::Clockwise);
+    fn act(&mut self, context: &Context) -> Action {
+        // Evaluate the current state
+        let threat_level = self.evaluate_threat(context);
+        let opportunity_level = self.evaluate_opportunity(context);
+
+        // Decision tree based on state evaluation
+        if threat_level >= opportunity_level {
+            self.defensive_action(context)
+        } else if opportunity_level > 0 {
+            self.offensive_action(context)
         } else {
-            self.fired = true;
-            return Action::Fire;
+            self.exploratory_action(context)
         }
     }
 
     fn name(&self) -> String {
-        "tantti".to_string()
+        "Tantti".to_string()
     }
 }
