b16169facf6e7cf553be5d3c53fd88198d0517d1
Antti Terävä <antti.terava@wapice.com>
2025-01-24 14:09:46 +0200

    Threat and opportunity evaluation during meeting.

--- src/players/terava/mod.rs
+++ src/players/terava/mod.rs
@@ -1,18 +1,54 @@
 use super::player::*;
 
-pub struct PlAgiAntti {}
+struct EnemyInfo {
+    timestamp: u32,
+    position: Position,
+    max_health: u32,
+}
+pub struct PlAgiAntti {
+    game_time: u32,
+    terrain_map: Vec<Vec<Option<Terrain>>>,
+    enemies: Vec<EnemyInfo>,
+}
 
 impl PlAgiAntti {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            game_time: 0,
+            terrain_map: Vec::new(),
+            enemies: Vec::new(),
+        }
     }
 
-    fn evaluate_threat(&self, _context: &Context) -> u32 {
-        // TODO: Implement logic to assess the threat level
-        rand::random::<u32>() % 2
+    fn evaluate_threat(&self, context: &Context) -> u32 {
+        // Check if there are any enemies in the vicinity.
+        let mut threat_level = 0;
+        for enemy in &self.enemies {
+            let time_since_observed = self.game_time - enemy.timestamp;
+            if time_since_observed < 5 && context.position().manhattan_distance(&enemy.position) < SCANNING_DISTANCE {
+                threat_level += 1;
+            }
+        }
+        return threat_level;
     }
 
-    fn evaluate_opportunity(&self, _context: &Context) -> u32 {
+    fn evaluate_opportunity(&self, context: &Context) -> u32 {
+        // Check if we know the position of any enemies.
+        let mut opportunity_level = 0;
+        for enemy in &self.enemies {
+            if enemy.timestamp == self.game_time - 1 {
+                // We know where the enemy was on previous turn.
+                let enemy_distance = context.position().pythagorean_distance(&enemy.position);
+                if enemy_distance < POSITIONAL_AIMING_DISTANCE {
+                    // The enemy is close enough for positional aiming.
+                    opportunity_level += 10;
+                } else if enemy_distance < SCANNING_DISTANCE  &&
+                Orientation::direction_towards(&context.position(), &enemy.position) == context.orientation() {
+                    // The enemy is close enough for cardinal aiming and in the correct direction.
+                    opportunity_level += 10;
+                }
+            }
+        }
         // TODO: Implement logic to assess the opportunity level
         rand::random::<u32>() % 2
     }
@@ -35,6 +71,13 @@ impl PlAgiAntti {
 
 impl Player for PlAgiAntti {
     fn act(&mut self, context: &Context) -> Action {
+        if self.terrain_map.is_empty() {
+            // Initialize the terrain map.
+            self.terrain_map = vec![vec![None; MAX_WORLD_SIZE]; MAX_WORLD_SIZE];
+        }
+        // Keep track of game time.
+        self.game_time += 1;
+
         // Evaluate the current state
         let threat_level = self.evaluate_threat(context);
         let opportunity_level = self.evaluate_opportunity(context);
@@ -51,5 +94,9 @@ impl Player for PlAgiAntti {
 
     fn name(&self) -> String {
         "Tantti".to_string()
+    }
+
+    fn is_ready(&self) -> bool {
+        true
     }
 }
