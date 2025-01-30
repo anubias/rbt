dd330d746533525e020447d3b7cce98613164f9e
Antti Arola <196191124+anttiarola@users.noreply.github.com>
2025-01-30 15:09:24 +0200

    Arola: Add high level player state (Explore, Attack)

--- src/players/arola/mod.rs
+++ src/players/arola/mod.rs
@@ -1,24 +1,117 @@
 use super::player::*;
 
-pub struct Arola {}
+pub struct Arola {
+    state: PlayerState,
+}
+
+enum PlayerState {
+    Explore,
+    Attack,
+}
 
 impl Arola {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            state: PlayerState::Explore,
+        }
     }
 }
 
 impl Player for Arola {
     fn act(&mut self, context: Context) -> Action {
-        match context.scanned_data() {
-            None => Action::Scan(ScanType::Mono(context.orientation().clone())),
-            Some(scan_data) => get_moving_action(scan_data, context.orientation()),
-        }
+        let (next_state, action) = match self.state {
+            PlayerState::Explore => explore(&context),
+            PlayerState::Attack => attack(&context),
+        };
+
+        self.state = next_state;
+        return action;
     }
 
     fn name(&self) -> String {
         "Arola".to_string()
     }
+
+    fn is_ready(&self) -> bool {
+        false
+    }
+}
+
+fn explore(context: &Context) -> (PlayerState, Action) {
+    // Effectively scan every second round and act every second
+    match context.scanned_data() {
+        None => request_scan(context),
+        Some(_) => process_scan_and_act(context),
+    }
+}
+
+fn request_scan(context: &Context) -> (PlayerState, Action) {
+    (
+        PlayerState::Explore,
+        Action::Scan(ScanType::Mono(context.orientation().clone())),
+    )
+}
+
+fn process_scan_and_act(context: &Context) -> (PlayerState, Action) {
+    if let Some(scan_data) = context.scanned_data() {
+        if find_other_players(scan_data) {
+            attack(context)
+        } else {
+            (
+                PlayerState::Explore,
+                get_moving_action(scan_data, context.orientation()),
+            )
+        }
+    } else {
+        panic!("No scan data available")
+    }
+}
+
+fn get_my_position_in_scan(scan_data: &ScanResult) -> (usize, usize) {
+    match &scan_data.scan_type {
+        ScanType::Mono(orientation) => match orientation {
+            Orientation::North => (SCANNING_DISTANCE - 1, SCANNING_DISTANCE / 2),
+            Orientation::NorthEast => (SCANNING_DISTANCE - 1, 0),
+            Orientation::East => (SCANNING_DISTANCE / 2, 0),
+            Orientation::SouthEast => (0, 0),
+            Orientation::South => (0, SCANNING_DISTANCE / 2),
+            Orientation::SouthWest => (0, SCANNING_DISTANCE - 1),
+            Orientation::West => (SCANNING_DISTANCE / 2, SCANNING_DISTANCE - 1),
+            Orientation::NorthWest => (SCANNING_DISTANCE - 1, SCANNING_DISTANCE - 1),
+        },
+        ScanType::Omni => (SCANNING_DISTANCE / 2, SCANNING_DISTANCE / 2),
+    }
+}
+
+fn find_other_players(scan_data: &ScanResult) -> bool {
+    let my_position = get_my_position_in_scan(scan_data);
+
+    for i in 1..scan_data.data.len() - 1 {
+        for j in 1..scan_data.data[i].len() - 1 {
+            if matches!(scan_data.data[i][j], MapCell::Player(_, _)) && (i, j) != my_position {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+fn attack(context: &Context) -> (PlayerState, Action) {
+    if let Some(scan_data) = context.scanned_data() {
+        if find_other_players(scan_data) {
+            println!("Attack");
+            return (
+                PlayerState::Attack,
+                Action::Scan(ScanType::Mono(context.orientation().clone())),
+            );
+        }
+    }
+
+    return (
+        PlayerState::Explore,
+        Action::Scan(ScanType::Mono(context.orientation().clone())),
+    );
 }
 
 fn get_moving_action(scan_data: &ScanResult, heading: &Orientation) -> Action {
@@ -34,7 +127,7 @@ fn get_moving_action(scan_data: &ScanResult, heading: &Orientation) -> Action {
     };
 
     match front_cell {
-        MapCell::Player(_, _) => Action::Move(Direction::Forward), // Crash to the other player
+        MapCell::Player(_, _) => Action::Fire(Aiming::Cardinal(heading.clone())),
         MapCell::Terrain(Terrain::Field) => Action::Move(Direction::Forward),
         MapCell::Terrain(_) => Action::Rotate(Rotation::Clockwise),
         MapCell::Unknown => Action::Idle,
