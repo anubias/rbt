3c6d62708f78d09b56be15e111488257e7ab2ac6
Antti Arola <196191124+anttiarola@users.noreply.github.com>
2025-02-07 12:14:42 +0200

    Arola: Split player state logic to multiple files and scan in radar style

--- src/players/arola/scan_helper.rs
+++ src/players/arola/scan_helper.rs
@@ -1,3 +1,4 @@
+use super::super::super::DEAD_AVATAR;
 use super::super::player::*;
 
 const SCAN_TOP: usize = 0;
@@ -7,7 +8,7 @@ const SCAN_BOTTOM: usize = SCANNING_DISTANCE - 1;
 const SCAN_RIGHT: usize = SCANNING_DISTANCE - 1;
 
 impl ScanResult {
-    pub fn get_my_position(&self) -> Position {
+    fn get_my_position(&self) -> Position {
         match &self.scan_type {
             ScanType::Mono(orientation) => match orientation {
                 Orientation::North => Position {
@@ -50,28 +51,33 @@ impl ScanResult {
         }
     }
 
+    fn get_player_id(map_cell: &MapCell) -> PlayerId {
+        match map_cell {
+            MapCell::Explosion(player_id, _) => player_id.clone(),
+            MapCell::Player(player_id, _) => player_id.clone(),
+            MapCell::Shell(player_id, _) => player_id.clone(),
+            _ => INVALID_PLAYER_ID,
+        }
+    }
+
     pub fn find_other_players(
         &self,
         my_id: &PlayerId,
         my_world_position: &Position,
     ) -> Vec<(PlayerId, Position)> {
-        let mut players = Vec::new();
-        let scan_position = self.get_world_position(my_world_position);
+        let mut other_players = Vec::new();
+        let scan_world_position = self.get_world_position(my_world_position);
 
         for y in 0..self.data.len() {
             for x in 0..self.data[y].len() {
-                let player_id = match self.data[y][x] {
-                    MapCell::Explosion(player_id, _) => player_id,
-                    MapCell::Player(player_id, _) => player_id,
-                    MapCell::Shell(player_id, _) => player_id,
-                    _ => INVALID_PLAYER_ID,
-                };
+                let player_id = ScanResult::get_player_id(&self.data[y][x]);
 
-                if player_id != INVALID_PLAYER_ID && &player_id != my_id {
-                    let position_x = x as isize + scan_position.0;
-                    let position_y = y as isize + scan_position.1;
+                if player_id != INVALID_PLAYER_ID && &player_id != my_id && player_id.is_alive() {
+                    let position_x = x as isize + scan_world_position.0;
+                    let position_y = y as isize + scan_world_position.1;
                     assert!(position_x >= 0 && position_y >= 0);
-                    players.push((
+
+                    other_players.push((
                         player_id,
                         Position {
                             x: position_x as usize,
@@ -82,14 +88,20 @@ impl ScanResult {
             }
         }
 
-        return players;
+        return other_players;
     }
 
-    pub fn get_world_position(&self, player_world_position: &Position) -> (isize, isize) {
-        let player_scan_position = self.get_my_position();
-        let scan_world_x = player_world_position.x as isize - player_scan_position.x as isize;
-        let scan_world_y = player_world_position.y as isize - player_scan_position.y as isize;
+    pub fn get_world_position(&self, my_world_position: &Position) -> (isize, isize) {
+        let my_scan_position = self.get_my_position();
+        let scan_world_x = my_world_position.x as isize - my_scan_position.x as isize;
+        let scan_world_y = my_world_position.y as isize - my_scan_position.y as isize;
 
         return (scan_world_x, scan_world_y);
+    }
+}
+
+impl PlayerId {
+    pub fn is_alive(&self) -> bool {
+        self.avatar != DEAD_AVATAR
     }
 }
