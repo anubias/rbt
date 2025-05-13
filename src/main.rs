b585f6b5fd5d2a7be87abd0eb80255d45a68ed84
Aurelian Pop <8299590+anubias@users.noreply.github.com>
2025-05-13 23:02:01 +0300

    Switching default championship to Open

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ const GAME_ROUNDS: u32 = 1;
 const WORLD_SIZE: WorldSize = WorldSize { x: 60, y: 45 };
 
 fn main() {
-    let mut championship = Championship::new(League::Academy, WORLD_SIZE);
+    let mut championship = Championship::new(League::Open, WORLD_SIZE);
     let championship_outcome = championship.run(GAME_ROUNDS);
 
     println!("");
