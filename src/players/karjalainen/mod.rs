0d07edd12255d3842dc165847afe23f6609dd43f
Miklas Karjalainen <miklas.karjalainen@gmail.com>
2025-05-15 20:24:09 +0300

    exploration added

--- src/players/karjalainen/mod.rs
+++ src/players/karjalainen/mod.rs
@@ -1,6 +1,10 @@
 mod evaluator;
 mod utils;
 
+use crate::api::map_cell::MapCell;
+use crate::api::player;
+use crate::api::scan::ScanResult;
+use crate::api::world_size::MAX_WORLD_SIZE;
 use crate::api::{
     self, action::Action, aiming::Aiming, context::Context, direction::Direction, map_cell,
     orientation::Orientation, player::Player, scan::ScanType,
@@ -12,13 +16,15 @@ use crate::api::rotation::Rotation;
 use evaluator::Evaluator;
 
 pub struct Miklas {
-    discovered_map: Vec<map_cell::MapCell>,
+    discovered_map: Box<[[MapCell; MAX_WORLD_SIZE]; MAX_WORLD_SIZE]>,
+    update_map: Box<[[usize; MAX_WORLD_SIZE]; MAX_WORLD_SIZE]>, // which round the tile was last updated.
 }
 
 impl Miklas {
     pub fn new() -> Self {
         Self {
-            discovered_map: vec![],
+            discovered_map: Box::new([[MapCell::Unallocated; MAX_WORLD_SIZE]; MAX_WORLD_SIZE]),
+            update_map: Box::new([[0; MAX_WORLD_SIZE]; MAX_WORLD_SIZE]),
         }
     }
 
@@ -29,43 +35,91 @@ impl Miklas {
 
         // Forward
         if let Some(pos) = self_pos.follow(&orientation, world_size) {
-            let eval = Evaluator::evaluate_position(world_size, &pos);
-            moves.push((Action::Move(Direction::Forward), eval));
-        }
-        // Backward
-        if let Some(pos) = self_pos.follow(&orientation.opposite(), world_size) {
-            let eval = Evaluator::evaluate_position(world_size, &pos);
-            moves.push((Action::Move(Direction::Backward), eval));
-        }
-    }
-
-    fn append_rotate(&self, context: &Context, moves: &mut Vec<(Action, i64)>) {
-        let self_pos = context.position();
-        let orientation = context.player_details().orientation;
-        let world_size = context.world_size();
-
-        let best_adjacent_position = self_pos
-            .list_adjacent_positions(world_size)
-            .into_iter()
-            .map(|pos| (pos.clone(), Evaluator::evaluate_position(world_size, &pos)))
-            .max_by_key(|(_, eval)| *eval);
-
-        if let Some((pos, pos_eval)) = best_adjacent_position {
-            let target_orientation = self_pos.get_orientation_to_pos(&pos);
-            let (rot, rot_turns) = orientation.quick_turn(&target_orientation);
-            let turn_eval = pos_eval - ((rot_turns * 10) as i64);
-            moves.push((Action::Rotate(rot), turn_eval));
+            match self.discovered_map[pos.y][pos.x] {
+                MapCell::Unallocated => {
+                    moves.push((Action::Scan(ScanType::Mono(orientation)), 500));
+                }
+                MapCell::Terrain(terrain) => {
+                    if terrain == map_cell::Terrain::Field {
+                        let eval = Evaluator::evaluate_position(world_size, &pos);
+                        moves.push((Action::Move(Direction::Forward), eval));
+                    } else {
+                        moves.push((Action::Rotate(Rotation::Clockwise), 500));
+                    }
+                }
+                _ => {}
+            }
         }
     }
 
-    fn choose_action(&self, context: &Context) -> Action {
+    fn parse_scan(&mut self, context: &Context, result: &ScanResult) {
+        let (width, height) = (context.world_size().x, context.world_size().y);
+
+        // find the local player use it as offset
+        let player_in_map: (usize, usize) = {
+            let mut found = false;
+            let mut out = (0, 0);
+            for y in 0..SCANNING_DISTANCE {
+                if found {
+                    break;
+                }
+
+                for x in 0..SCANNING_DISTANCE {
+                    if let MapCell::Player(details, _) = result.data[y][x] {
+                        if details.id == context.player_details().id {
+                            out = (x, y);
+                            found = true;
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+            }
+            out
+        };
+
+        let offset = (
+            (context.position().x as i32) - (player_in_map.0 as i32),
+            (context.position().y as i32) - (player_in_map.1 as i32),
+        );
+
+        for y in 0..SCANNING_DISTANCE {
+            for x in 0..SCANNING_DISTANCE {
+                let map = (offset.0 + x as i32, offset.1 + y as i32);
+                if !(0..MAX_WORLD_SIZE).contains(&(map.0 as usize))
+                    || !(0..MAX_WORLD_SIZE).contains(&(map.1 as usize))
+                {
+                    continue;
+                }
+
+                self.discovered_map[map.1 as usize][map.0 as usize] = result.data[y][x];
+                self.update_map[map.1 as usize][map.0 as usize] = context.turn();
+            }
+        }
+
+        // print
+        for y in 0..height {
+            for x in 0..width {
+                print!("{}", self.discovered_map[y][x]);
+            }
+            print!("\n");
+        }
+    }
+
+    fn append_scan(&mut self, context: &Context, moves: &mut Vec<(Action, i64)>) {
+        if let Some(result) = context.scanned_data() {
+            self.parse_scan(context, result);
+        }
+    }
+
+    fn choose_action(&mut self, context: &Context) -> Action {
         use rand::distributions::{Distribution, WeightedIndex};
 
         // Fetch all possible actions and assign a weight for each one
         let mut moves: Vec<(Action, i64)> = vec![];
         self.append_move(context, &mut moves);
-        self.append_rotate(context, &mut moves);
-        moves.sort_by_key(|&(_, weight)| weight);
+        self.append_scan(context, &mut moves);
+        moves.sort_by_key(|&(_, weight)| -weight);
 
         // Take top-three and randomly choose on by their weight.
         let top_three = moves
@@ -78,11 +132,21 @@ impl Miklas {
             .map(|(_, weight)| *weight)
             .collect::<Vec<i64>>();
 
-        let dist = WeightedIndex::new(&weights).unwrap();
+        let dist = match WeightedIndex::new(&weights) {
+            Ok(some) => some,
+            Err(_) => {
+                return Action::Scan(ScanType::Omni); // fallback if no actions are available.
+            }
+        };
         let mut rng = rand::thread_rng();
 
         let select_index = dist.sample(&mut rng);
-        println!("{:?}\n\nchose{}", moves, select_index);
+        println!(
+            "{:?}\n\nchose{} orientation {}",
+            moves,
+            select_index,
+            context.player_details().orientation
+        );
         top_three[select_index].0.clone()
     }
 }
@@ -96,8 +160,6 @@ impl Player for Miklas {
                 Evaluator::evaluate_position(context.world_size(), context.position())
             )
         }
-
-        Action::Scan(ScanType::Mono(Orientation::East));
         self.choose_action(&context)
     }
 
