28a6668dd0ea1b8e6f6c3e4aa7e80b7fe5730138
Miklas Karjalainen <miklas.karjalainen@gmail.com>
2025-05-15 14:26:26 +0300

    beginnings of the ultimate destroyer ai.

---
+++ src/players/karjalainen/mod.rs
@@ -0,0 +1,62 @@
+use crate::api::{
+    action::Action, aiming::Aiming, context::Context, direction::Direction,
+    orientation::Orientation, player::Player, scan::ScanType,
+};
+
+pub struct Miklas {}
+
+impl Miklas {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    fn evaluate_threat(&self, _context: &Context) -> u32 {
+        // TODO: Implement logic to assess the threat level
+        rand::random::<u32>() % 2
+    }
+
+    fn evaluate_opportunity(&self, _context: &Context) -> u32 {
+        // TODO: Implement logic to assess the opportunity level
+        rand::random::<u32>() % 2
+    }
+
+    fn defensive_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for defensive actions
+        Action::Move(Direction::Backward)
+    }
+
+    fn offensive_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for offensive actions
+        Action::Fire(Aiming::Cardinal(Orientation::default()))
+    }
+
+    fn exploratory_action(&self, _context: &Context) -> Action {
+        // TODO: Implement logic for exploratory actions
+        Action::Scan(ScanType::Mono(Orientation::East))
+    }
+}
+
+impl Player for Miklas {
+    fn act(&mut self, context: Context) -> Action {
+        // Evaluate the current state
+        let threat_level = self.evaluate_threat(&context);
+        let opportunity_level = self.evaluate_opportunity(&context);
+
+        // Decision tree based on state evaluation
+        if threat_level >= opportunity_level {
+            self.defensive_action(&context)
+        } else if opportunity_level > 0 {
+            self.offensive_action(&context)
+        } else {
+            self.exploratory_action(&context)
+        }
+    }
+
+    fn name(&self) -> String {
+        "Miklas".to_string()
+    }
+
+    fn is_ready(&self) -> bool {
+        true
+    }
+}
